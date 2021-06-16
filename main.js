9942f85000331e27e421e177ad4581844114c197
tdw535 <tkedwards0@gmail.com>
2021-06-15 20:08:51 -0600

    Testing webgl tutorial

---
+++ main.js
@@ -0,0 +1,103 @@
+//From tutorial
+const canvas = document.querySelector('canvas');
+const gl = canvas.getContext('webgl');
+
+if(!gl){
+    throw new Error(`WebGL not supported`);
+}
+
+
+
+
+const vertexData = [
+    0,1,0,
+    1,-1,0,
+    -1,-1,0,
+];
+
+const colorData = [
+    1,0,0,
+    0,1,0,
+    0,0,1,
+];
+
+
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+const colorBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
+
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader,`
+precision mediump float;
+
+attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
+
+uniform mat4 matrix;
+
+void main() {
+    vColor = color;
+    gl_Position = matrix * vec4(position,1);
+}
+`);
+gl.compileShader(vertexShader);
+
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fragmentShader, `
+precision mediump float;
+
+varying vec3 vColor;
+
+void main() {
+    gl_FragColor = vec4(vColor,1);
+}
+`);
+gl.compileShader(fragmentShader);
+
+const program = gl.createProgram();
+gl.attachShader(program,vertexShader);
+gl.attachShader(program,fragmentShader);
+gl.linkProgram(program);
+
+const positionLocation = gl.getAttribLocation(program,`position`);
+gl.enableVertexAttribArray(positionLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT,false,0,0);
+
+const colorLocation = gl.getAttribLocation(program,`color`);
+gl.enableVertexAttribArray(colorLocation);
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT,false,0,0);
+
+gl.useProgram(program);
+
+const uniformLocations = {
+    matrix : gl.getUniformLocation(program, `matrix`),
+}
+
+const matrix = glMatrix.mat4.create();
+glMatrix.mat4.translate(matrix,matrix,[.2,.5,0]);
+glMatrix.mat4.scale(matrix,matrix,[0.4,0.4,0.4]);
+
+function animate() {
+    requestAnimationFrame(animate);
+    glMatrix.mat4.rotateZ(matrix,matrix,Math.PI/2/70);
+    glMatrix.mat4.rotateX(matrix,matrix,Math.PI/2/50);
+
+    gl.uniformMatrix4fv(uniformLocations.matrix,false,matrix);
+    gl.drawArrays(gl.TRIANGLES,0,3);
+
+}
+
+animate()
+
+
+
